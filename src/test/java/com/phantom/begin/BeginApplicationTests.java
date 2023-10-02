ef84cbf8e660749f0d2883042ca3b5645b9aa55f
RC-Regalado <ruben.alekey@gmail.com>
2023-10-01 21:28:06 -0600

    Punto de inicio de Spring

---
+++ src/test/java/com/phantom/begin/BeginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantom.begin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
